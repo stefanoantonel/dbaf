96fadf3b1c02db6209c1addc4b112159092e2c69
stefanoantonel <stefanoantonel@gmail.com>
2013-11-14 15:08:35 -0300

    cree interfaz

---
+++ src/ar/edu/ucc/bda/web/modelo/dao/INotasDAO.java
@@ -0,0 +1,28 @@
+package ar.edu.ucc.bda.web.modelo.dao;
+
+import java.sql.Connection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public interface INotasDAO {
+	
+	public JSONArray load(String usuario) throws JSONException;
+	
+	public JSONArray loadUltima() throws JSONException;
+	
+	public JSONArray load(String usuario,String notaParaBuscar) throws JSONException;
+	
+	public boolean updateTitulo(String titulo,String id);
+	
+	public boolean updateCuerpo(String cuerpo,String id);
+	
+	public boolean updateLista(String lista, String id);
+
+	public boolean insert(String usuario);
+	
+	public boolean delete(String id);
+	
+	
+}
